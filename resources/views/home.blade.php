d460cd516638b1b97ec5ba2e2ddcadc8724b3638
hsulaenandar <hsulaenandar1998@gmail.com>
2022-08-19 11:09:50 +0630

    old data

---
+++ resources/views/home.blade.php
@@ -0,0 +1,235 @@
+@extends('layouts.frontend.master')
+@section('title')
+SHM Store| Home
+@endsection
+@section('content')
+
+  <button onclick="topFunction()" id="myBtn" class="myBtn" title="Go to top">
+    <img src="frontend/img/home/img_up_arrow5.jpg" alt="UpArrow" class="up-img">
+  </button>
+
+  <section class="slider-row">
+    <ul class="banner-slider pc">      
+      <li><img class="pc" src="frontend/img/home/img_bg_18.jpg" alt="bgimage"></li>
+      <li><img class="pc" src="frontend/img/home/img_bg14.jpg" alt="bgimage"></li>
+      <li><img class="pc" src="frontend/img/home/img_bg_02.jpg" alt="bgimage"></li>
+    </ul>
+  </section> <!-- /.slider-row -->
+
+  <section class="laptop-row">
+    <div class="l-inner">
+      <div class="home-ttl">
+        <h2 class="cmn-h2">Hot Best Seller(Laptop)</h2>
+      </div>
+    
+      <ul class="home-product clearfix"> 
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_laptop3.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_laptop3.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_laptop3.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_laptop3.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_laptop3.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section> <!-- /.laptop-row -->
+
+  <section class="mobile-row">
+    <div class="l-inner">
+      <div class="home-ttl">
+        <h2 class="cmn-h2">Hot Best Seller(Mobile)</h2>
+      </div>
+    
+      <ul class="home-product clearfix">
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_ph4.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_ph4.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_ph4.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_ph4.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section> <!-- /.mobile-row -->
+
+  <section class="watch-row">
+    <div class="l-inner">
+      <div class="home-ttl">
+        <h2 class="cmn-h2">Hot Best Seller(Watch)</h2>
+      </div>
+    
+      <ul class="home-product clearfix">
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_watch5.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_watch3.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_watch4.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+
+        <li class="home-pcol1">
+          <img src="frontend/img/home/img_watch1.jpg" alt="HP 15s-eq2113AU ( Natural Silver )">
+          <div class="home-pcol1-txt">
+            <p class="cmn-p"><sup><del>1,000,000 MMK</del></sup>1,735,750 MMK</p>
+            <h5 class="cmn-h5">HP 15s-eq2113AU ( Natural Silver )</h5>
+            <button class="add-to-cart-btn">Add to cart</button>
+            <button class="viewmore">View More</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section> <!-- /.watch-row -->
+
+  <section class="review-row">
+    <div class="l-inner">
+      <h2 class="cmn-h2">Reviews</h2>
+      <div class="home-review clearfix">
+        <div class="review-col">
+          <h3 class="cmn-h3">John</h3>
+          <p class="cmn-p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sit necessitatibus consequuntur, et alias cumque laboriosam aspernatur magnam unde quibusdam ex, id rerum eius dolore labore asperiores, optio placeat est?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sit necessitatibus consequuntur, et alias cumque laboriosam aspernatur magnam unde quibusdam ex, id rerum eius dolore labore asperiores, optio placeat est?
+            
+          </p>
+        </div>
+
+        <div class="review-col">
+          <h3 class="cmn-h3">John</h3>
+          <p class="cmn-p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sit necessitatibus consequuntur, et alias cumque laboriosam aspernatur magnam unde quibusdam ex, id rerum eius dolore labore asperiores, optio placeat est?
+           </p>
+        </div>
+
+        <div class="review-col">
+          <h3 class="cmn-h3">John</h3>
+          <p class="cmn-p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sit necessitatibus consequuntur, et alias cumque laboriosam aspernatur magnam unde quibusdam ex, id rerum eius dolore labore asperiores, optio placeat est?
+          </p>
+        </div>
+        <div class="review-col">
+          <h3 class="cmn-h3">John</h3>
+          <p class="cmn-p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sit necessitatibus consequuntur, et alias cumque laboriosam aspernatur magnam unde quibusdam ex, id rerum eius dolore labore asperiores, optio placeat est?
+          </p>
+        </div>
+      </div>
+    </div>
+  </section> <!-- /.review-row -->
+
+  
+  <script>
+    var mybutton = document.getElementById("myBtn");
+    
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+    
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+  </script>
+    
+@endsection

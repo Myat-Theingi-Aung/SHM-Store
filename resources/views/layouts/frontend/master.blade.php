c14c06dc3db66c20b6317ef8bc42b1c1a3b5dc4d
Myat-Theingi-Aung <myattheingiaung1720@gmail.com>
2022-08-26 01:49:19 +0630

    update

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -12,18 +12,10 @@
   <link rel="stylesheet" href="{{asset('frontend/css/common.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/home.css')}}">
-<<<<<<< HEAD
-
-  <link rel="stylesheet" href="{{asset('frontend/css/cart.css')}}">
-
-  <link rel="stylesheet" href="{{asset('frontend/css/feedback.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/product.css')}}">
-
-=======
   <link rel="stylesheet" href="{{asset('frontend/css/cart.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/feedback.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/product.css')}}">
->>>>>>> daf46a7a8d687c785101563732fc47994a53ec7e
+
   <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
 </head>
 

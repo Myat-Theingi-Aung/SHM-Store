3956cf9cbfdd0b21f4df8c03c80045b43b422813
MoeChan <moechan599@gmail.com>
2022-08-26 00:54:11 +0630

    nav-update

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -8,14 +8,20 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title')</title>
   <link rel="stylesheet" href="{{asset('frontend/css/reset.css')}}">
-  <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+  <link rel="stylesheet" href="{{asset('frontend/css/slick.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/common.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
- 
+  <link rel="stylesheet" href="{{asset('frontend/css/home.css')}}">
+
+  <link rel="stylesheet" href="{{asset('frontend/css/cart.css')}}">
+
+  <link rel="stylesheet" href="{{asset('frontend/css/feedback.css')}}">
+  <link rel="stylesheet" href="{{asset('frontend/css/product.css')}}">
+
+  <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
 </head>
 
 <body>
-
 
   <header class="sec-header">
     <div class="l-inner clearfix">
@@ -26,12 +32,35 @@
       <nav class="gnav">
         <ul>
           <li><a href="{{route('home')}}" class="{{  Request::is('/') ? 'active' : '' }}">Home</a></li>
-          <li><a href="" class="">Product</a></li>
-          <li><a href="{{route('about')}}" class="{{  Request::is('about') ? 'active' : '' }}">About</a></li>
-          <li><a href="" class="">Feedback</a></li>
-          <li><a href="" >Login</a></li>
+          <li><a href="{{route('product')}}" class="{{  Request::is('product') ? 'active' : '' }}">Product</a></li>
+          <li>
+            <a href="{{route('about')}}" class="{{  Request::is('about') ? 'active' : '' }}">About
+            </a>
+          </li>
+          <li><a href="{{route('feedback')}}" class="{{  Request::is('feedback') ? 'active' : '' }}">Feedback</a></li>
+          @auth
+          <li><a href="{{url('/logout')}}">Logout</a></li>
+          @else
+         
+          <li class="dropdown pc">
+            <button class="dropbtn pc"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M344.7 238.5l-144.1-136C193.7 95.97 183.4 94.17 174.6 97.95C165.8 101.8 160.1 110.4 160.1 120V192H32.02C14.33 192 0 206.3 0 224v64c0 17.68 14.33 32 32.02 32h128.1v72c0 9.578 5.707 18.25 14.51 22.05c8.803 3.781 19.03 1.984 26-4.594l144.1-136C354.3 264.4 354.3 247.6 344.7 238.5zM416 32h-64c-17.67 0-32 14.33-32 32s14.33 32 32 32h64c17.67 0 32 14.33 32 32v256c0 17.67-14.33 32-32 32h-64c-17.67 0-32 14.33-32 32s14.33 32 32 32h64c53.02 0 96-42.98 96-96V128C512 74.98 469 32 416 32z"/></svg></button>
+            <div class="dropdown-content pc">
+            <a href="{{route('login')}}">Login</a>
+          <a href="{{route('register')}}"  class="{{  Request::is('/register') ? 'active' : '' }}">Register</a>
+          </div>
+          
+          </li>
+          <li class="  sp">
+          <ul class="list">
+          <li><a href="{{route('register')}}"  class="{{  Request::is('/register') ? 'active' : '' }}  sp">Register</a></li>
+          <li class="sp"><a href="{{route('login')}} ap">Login</a>
+          </li>
+          </ul>
+          
+          
+          
+          @endauth
         </ul>
-
       </nav>
       <!--gnav-->
       <button class="menu-icon">
@@ -83,10 +112,19 @@
     <!--footer-->
   </section>
   <!--sec-footer-->
-  <script src="{{asset('frontend/js/libary/jquery-3.6.0.min.js')}}"></script>
+
+  <script src="{{asset('frontend/js/libary/jquery.min.js')}}"></script>
+  <script src="{{asset('frontend/js/libary/slick.min.js')}}"></script>
+  <script src="{{asset('frontend/js/libary/jquery.heightLine.js')}}"></script>
+  <script src="{{asset('frontend/js/slider.js')}}"></script>
+  <script src="{{asset('frontend/js/heightline.js')}}"></script>
   <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
   {!! Toastr::message() !!}
   <script src="{{asset('frontend/js/common.js')}}"></script>
+
+ 
+  
+
 </body>
 
 </html>
